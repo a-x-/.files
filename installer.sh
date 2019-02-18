b750d6921ac8be8f1156ac566813715b63ee913b
Alexander Mextner <a-x-@users.noreply.github.com>
2019-02-18 16:41:31 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# Standalone installer for Unixs
-# Original version is created by shoma2da
-# https://github.com/shoma2da/neobundle_installer
 
 # !WARN! INSTALLER IS NOT COMPLETE
 
 echo install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 . ~/.zshrc
+
+# todo: create some symlinks in `~`
+# install something else: https://github.com/a-x-/apps
 
 # install https://github.com/Shougo/dein.vim
