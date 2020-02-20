1baad5f490afeb57a901516507e4d31194f4ee63
Alexander Mextner <a-x-@users.noreply.github.com>
2020-02-20 18:03:25 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -2,13 +2,15 @@
 
 # !WARN! INSTALLER IS NOT COMPLETE
 
+# install something else: https://github.com/a-x-/apps
+
 xcode-select --install
+
+# todo: create some symlinks in `~`
+ln -s .files/.zshrc .files/.gitconfig .files/.tmux/ .files/.tmux.conf .files/.vimrc .files/.profile .
 
 echo install zplug
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 . ~/.zshrc
-
-# todo: create some symlinks in `~`
-# install something else: https://github.com/a-x-/apps
 
 # install https://github.com/Shougo/dein.vim
