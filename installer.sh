f4b1d8a6d71b92bf5491398522180e75f0530680
Alexander Mextner <a-x-@users.noreply.github.com>
2020-02-25 22:49:14 +0300

    fix ignore comments

--- installer.sh
+++ installer.sh
@@ -17,14 +17,13 @@ ln -s .files/.zshrc .files/.gitconfig .files/.tmux/ .files/.tmux.conf .files/.vi
 #
 #
 echo "\n\n install brew packages"
-IGNORE_COMMENTS='s/#.*//'
 brew install git && \
 mkdir ~/xp && cd ~/xp && \
 git clone https://github.com/a-x-/apps.git && cd apps && \
-cat ~/xp/apps/brew.list       | sed -E $IGNORE_COMMENTS | xargs brew install && \
-cat ~/xp/apps/brew-cask.list  | sed -E $IGNORE_COMMENTS | xargs brew cask install&& \
-cat ~/xp/apps/npm.list        | sed -E $IGNORE_COMMENTS | xargs npm -g install&& \
-cat ~/xp/apps/vscode.list     | sed -E $IGNORE_COMMENTS | while read f; do code --install-extension $f; done
+cat ~/xp/apps/brew.list       | sed -E '/^#|^$/d' | sed -E 's/#.*//' | xargs brew install && \
+cat ~/xp/apps/brew-cask.list  | sed -E '/^#|^$/d' | sed -E 's/#.*//' | xargs brew cask install&& \
+cat ~/xp/apps/npm.list        | sed -E '/^#|^$/d' | sed -E 's/#.*//' | xargs npm -g install&& \
+cat ~/xp/apps/vscode.list     | sed -E '/^#|^$/d' | sed -E 's/#.*//' | while read f; do code --install-extension $f; done
 
 
 #
